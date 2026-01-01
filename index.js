4c014f79d024087c38380b493f3438b6315a1bd6
aidankeighron <aidankeighron@gmail.com>
2026-01-01 13:17:09 -0600

    remove widgets

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 import notifee, { EventType } from '@notifee/react-native';
 import 'expo-router/entry';
-import { registerWidgetTaskHandler } from 'react-native-android-widget';
-import { widgetTaskHandlerWrapper } from './widget-task-handler';
-
-// Register the widget task handler
-registerWidgetTaskHandler(widgetTaskHandlerWrapper);
 
 notifee.onBackgroundEvent(async ({ type, detail }) => {
   const { notification, pressAction } = detail;
